fd3b205e16cd268e837ecef93a847ca74b708d72
muhammad Waqas <waqasakhyer@gmail.com>
2023-10-04 00:36:25 +0500

    Router

---
+++ 04-Practice/src/Components/Service/Service.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Service() {
+  return (
+    <div>
+      <h1>This is a Service</h1>
+    </div>
+  )
+}
+
+export default Service
--- 05-Router/src/main.jsx
+++ 05-Router/src/main.jsx
@@ -2,9 +2,29 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { Route, RouterProvider, createBrowserRouter, createRoutesFromElements } from 'react-router-dom'
+import Layout from './Layout.jsx'
+import Home from './components/Home/Home.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import Services from './components/Services/Services.jsx'
+import Contacts from './components/Contacts/Contacts.jsx'
+
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path='/' element={<Layout/>}>
+      <Route path='' element={<Home/>} />
+   
+      <Route path='services' element={<Services/>} />
+      <Route path='contacts' element={<Contacts/>} />
+
+
+    </Route>
+  )
+)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider  router={router}/>
   </React.StrictMode>,
 )
